d6ff3845e5221a0a538a61aa681c59d039fa720f
marinescu97 <marinescumonica97@yahoo.com>
2025-07-21 11:51:09 +0300

    Fixed book specification

--- src/main/java/com/store/Bookwire/specifications/BookSpecification.java
+++ src/main/java/com/store/Bookwire/specifications/BookSpecification.java
@@ -1,27 +1,30 @@
 package com.store.Bookwire.specifications;
 
 import com.store.Bookwire.models.entities.Book;
+import com.store.Bookwire.specifications.builder.FieldEqualsSpecificationBuilder;
+import com.store.Bookwire.specifications.builder.RangeSpecificationBuilder;
+import com.store.Bookwire.specifications.builder.SearchSpecificationBuilder;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class BookSpecification extends EntitySpecification<Book> {
+public class BookSpecification {
     public Specification<Book> titleAuthorIsbnContains(String value) {
-        return search(value, List.of("title", "author", "isbn"));
+        return new SearchSpecificationBuilder<Book>(value, List.of("title", "author", "isbn")).build();
     }
 
     public Specification<Book> hasCategory(List<String> categories) {
-        return fieldEquals(List.of("category"), categories);
+        return new FieldEqualsSpecificationBuilder<Book>(List.of("category"), categories).build();
     }
 
     public Specification<Book> priceBetween(Double min, Double max) {
-        return isBetween("price", min, max);
+        return new RangeSpecificationBuilder<Book, Double>("price", min, max).build();
     }
 
     public Specification<Book> pagesBetween(Integer min, Integer max) {
-        return isBetween("numberOfPages", min, max);
+        return new RangeSpecificationBuilder<Book, Integer>("numberOfPages", min, max).build();
     }
 }
 
